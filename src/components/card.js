47423556ae211c90f7153ee461bd6aef8f2e5c9f
Francesco Rizzi <rizzifrancesco@icloud.com>
2018-07-28 22:21:06 +0200

    RELATED BRANCH: master; MESSAGE: http to https images

--- src/components/card.js
+++ src/components/card.js
@@ -6,7 +6,7 @@ export default class Card extends Component {
 		const hero = this.props.hero;
 		
 		return (<div className='app-heroes-card' >
-			<div className="app-heroes-card-image" style={{backgroundImage: `url('${hero.thumbnail.path}.${hero.thumbnail.extension}')`}}></div>
+			<div className="app-heroes-card-image" style={{backgroundImage: `url('${hero.thumbnail.path.replace('http','https')}.${hero.thumbnail.extension}')`}}></div>
 			<div className="app-heroes-card-text">
 				<div className="app-heroes-card-title">{hero.name}</div>
 				<div className="app-heroes-card-saved" onClick={this.props.onToggleBookmark.bind(this, hero)}>
